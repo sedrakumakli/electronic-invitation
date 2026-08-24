d47e7c4d9c31ec98df1e547f27e923dfa48d9cbe
solaf10 <soso103m@gmail.com>
2026-08-24 08:56:46 +0300

    fix/deployment

--- src/components/GraduationMemories/GraduationMemories.tsx
+++ src/components/GraduationMemories/GraduationMemories.tsx
@@ -1,15 +1,7 @@
 import { motion, type Variants } from 'framer-motion';
 import { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
-import {
-  FaCamera,
-  FaHeart,
-  FaImage,
-  FaPaperPlane,
-  FaRegFaceSmile,
-  FaUpload,
-  FaXmark,
-} from 'react-icons/fa6';
+import { FaCamera, FaHeart, FaPaperPlane, FaXmark } from 'react-icons/fa6';
 import Container from '../Container';
 
 declare global {
